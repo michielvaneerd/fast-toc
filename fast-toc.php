ce9a2dba97f71921ee7d68607203a4b4521a665e
michielvaneerd <michiel.vaneerd@gmail.com>
2020-01-05 14:55:50 +0100

    Added setting selector to ignore specific headers

--- fast-toc.php
+++ fast-toc.php
@@ -11,6 +11,7 @@ add_action('wp_enqueue_scripts', function() {
             'show_toc' => get_post_meta(get_the_ID(), 'mve_show_toc', true) === 'true' ? true : false,
             'root_selector' => get_option('fast_toc_root_selector'),
             'title' => get_option('fast_toc_title'),
+            'fast_toc_selector_ignore' => get_option('fast_toc_selector_ignore')
         ];
         wp_add_inline_script('mve_toc', 'window.MVE_FAST_TOC=' . json_encode($jsVar) . ';', 'before');
 
@@ -45,6 +46,7 @@ add_action('enqueue_block_editor_assets', function() {
 add_action('admin_init', function() {
 
     register_setting('reading', 'fast_toc_root_selector');
+    register_setting('reading', 'fast_toc_selector_ignore');
     register_setting('reading', 'fast_toc_enabled');
     register_setting('reading', 'fast_toc_title');
 
@@ -82,6 +84,20 @@ add_action('admin_init', function() {
             ?>
             <input id="fast_toc_root_selector" type="text" name="fast_toc_root_selector" value="<?php echo isset( $setting ) ? esc_attr( $setting ) : ''; ?>">
             <p class="description">The CSS selector of the root element that wraps all the headers. If this is empty, the root element will be the BODY.</p>
+            <?php
+        },
+        'reading',
+        'fast_toc_settings_section'
+    );
+
+    add_settings_field(
+        'fast_toc_selector_ignore',
+        '<label for="fast_toc_selector_ignore">Ignore selector</label>',
+        function() {
+            $setting = get_option('fast_toc_selector_ignore');
+            ?>
+            <input id="fast_toc_selector_ignore" type="text" name="fast_toc_selector_ignore" value="<?php echo isset( $setting ) ? esc_attr( $setting ) : ''; ?>">
+            <p class="description">The CSS selector of headers that should NOT be added to the TOC. If this is empty, no header inside the root element is ignored.</p>
             <?php
         },
         'reading',
--- toc.js
+++ toc.js
@@ -1,12 +1,22 @@
 window.addEventListener("DOMContentLoaded", function() {
     if (MVE_FAST_TOC && MVE_FAST_TOC.show_toc) {
         var root = MVE_FAST_TOC.root_selector ? document.querySelector(MVE_FAST_TOC.root_selector) : document.body;
+
+        if (MVE_FAST_TOC.fast_toc_selector_ignore) {
+            root.querySelectorAll(MVE_FAST_TOC.fast_toc_selector_ignore).forEach(function(h) {
+                h.setAttribute("data-fast-toc-ignore", "true");
+            });
+        }
+
         var list = [];
         if (MVE_FAST_TOC.title) {
             list.push("<div class='fast-toc-title'>" + MVE_FAST_TOC.title + "</div>");
         }
         list.push("<ul>");
         root.querySelectorAll("h1, h2, h3, h4, h5, h6").forEach(function(h, index) {
+            
+            if (h.getAttribute('data-fast-toc-ignore')) return;
+
             var id = "fast-toc-" + index;
             list.push("<li class='fast-toc-" + h.nodeName.toLocaleLowerCase() + "'><a href='#" + id + "'>" + h.innerText + "</a></li>");
             h.setAttribute("id", id);
