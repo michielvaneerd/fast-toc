70b2f31157735e46d13a165984d7572dd27ded37
michielvaneerd <michiel.vaneerd@gmail.com>
2020-01-05 14:10:23 +0100

    First commit

---
+++ fast-toc.php
@@ -0,0 +1,106 @@
+<?php
+/*
+Plugin Name: Fast TOC
+*/
+
+add_action('wp_enqueue_scripts', function() {
+    if (is_singular()) {
+
+        wp_enqueue_script('mve_toc', plugin_dir_url(__FILE__) . 'toc.js');
+        $jsVar = [
+            'show_toc' => get_post_meta(get_the_ID(), 'mve_show_toc', true) === 'true' ? true : false,
+            'root_selector' => get_option('fast_toc_root_selector'),
+            'title' => get_option('fast_toc_title'),
+        ];
+        wp_add_inline_script('mve_toc', 'window.MVE_FAST_TOC=' . json_encode($jsVar) . ';', 'before');
+
+        wp_enqueue_style('mve_toc', plugin_dir_url(__FILE__) . 'toc.css');
+    }
+});
+
+add_action('init', function() {
+
+    register_post_meta('post', 'mve_show_toc', array(
+        'show_in_rest' => true,
+        'single' => true,
+        'type' => 'string', // 'true' or 'false' as strings, empty string means not set yet
+    ));
+
+    $asset_file = include(plugin_dir_path(__FILE__) . 'build/index.asset.php');
+    wp_register_script(
+        'plugin-sidebar-js',
+        plugins_url('/build/index.js', __FILE__),
+        $asset_file['dependencies'],
+        $asset_file['version']
+    );
+    $enabledByDefault = get_option('fast_toc_enabled') == 1;
+    wp_add_inline_script('plugin-sidebar-js', 'window.MVE_FAST_TOC_ENABLED=' . json_encode($enabledByDefault) . ';', 'before');
+
+});
+
+add_action('enqueue_block_editor_assets', function() {
+    wp_enqueue_script('plugin-sidebar-js');
+});
+
+add_action('admin_init', function() {
+
+    register_setting('reading', 'fast_toc_root_selector');
+    register_setting('reading', 'fast_toc_enabled');
+    register_setting('reading', 'fast_toc_title');
+
+    add_settings_section(
+        'fast_toc_settings_section',
+        'Fast TOC settings',
+        function() {},
+        'reading'
+    );
+
+    add_settings_field(
+        'fast_toc_enabled',
+        'Enable',
+        function() {
+            $setting = get_option('fast_toc_enabled');
+            ?>
+            <fieldset>
+            <label>
+            <input id="fast_toc_enabled" type="checkbox" name="fast_toc_enabled" <?php checked($setting, '1'); ?> value="1">
+            Enable by default
+            </label>
+            <p class="description">You can overrule this per post.</p>
+            </fieldset>
+            <?php
+        },
+        'reading',
+        'fast_toc_settings_section'
+    );
+ 
+    add_settings_field(
+        'fast_toc_root_selector',
+        '<label for="fast_toc_root_selector">Root selector</label>',
+        function() {
+            $setting = get_option('fast_toc_root_selector');
+            ?>
+            <input id="fast_toc_root_selector" type="text" name="fast_toc_root_selector" value="<?php echo isset( $setting ) ? esc_attr( $setting ) : ''; ?>">
+            <p class="description">The CSS selector of the root element that wraps all the headers. If this is empty, the root element will be the BODY.</p>
+            <?php
+        },
+        'reading',
+        'fast_toc_settings_section'
+    );
+
+    add_settings_field(
+        'fast_toc_title',
+        '<label for="fast_toc_title">Title</label>',
+        function() {
+            $setting = get_option('fast_toc_title');
+            ?>
+            <input id="fast_toc_title" type="text" name="fast_toc_title" value="<?php echo isset( $setting ) ? esc_attr( $setting ) : ''; ?>">
+            <p class="description">Leave empty to hide the title.</p>
+            <?php
+        },
+        'reading',
+        'fast_toc_settings_section'
+    );
+
+    
+});
---
+++ toc.js
@@ -0,0 +1,21 @@
+window.addEventListener("DOMContentLoaded", function() {
+    if (MVE_FAST_TOC && MVE_FAST_TOC.show_toc) {
+        var root = MVE_FAST_TOC.root_selector ? document.querySelector(MVE_FAST_TOC.root_selector) : document.body;
+        var list = [];
+        if (MVE_FAST_TOC.title) {
+            list.push("<div class='fast-toc-title'>" + MVE_FAST_TOC.title + "</div>");
+        }
+        list.push("<ul>");
+        root.querySelectorAll("h1, h2, h3, h4, h5, h6").forEach(function(h, index) {
+            var id = "fast-toc-" + index;
+            list.push("<li class='fast-toc-" + h.nodeName.toLocaleLowerCase() + "'><a href='#" + id + "'>" + h.innerText + "</a></li>");
+            h.setAttribute("id", id);
+        });
+        list.push("</ul>");
+        var el = document.createElement("div");
+        el.className = "fast-toc";
+        el.innerHTML = list.join("\n");
+        root.insertBefore(el, root.firstChild);
+    }
+
+});
