3f8cd5b163639bdc85211ca392ff3fc7739fa7fb
Michiel van Eerd <michiel.vaneerd@gmail.com>
2020-01-15 14:12:52 +0100

    Nested lists

--- toc.js
+++ toc.js
@@ -13,18 +13,67 @@ window.addEventListener("DOMContentLoaded", function() {
         if (FAST_TOC.title) {
             list.push("<div class='fast-toc-title'>" + FAST_TOC.title + "</div>");
         }
-        var hCounter = 0;
-        list.push("<ul>");
-        root.querySelectorAll("h1, h2, h3, h4, h5, h6").forEach(function(h, index) {
-            
-            if (h.getAttribute('data-fast-toc-ignore')) return;
 
-            var id = "fh-" + index;
-            list.push("<li class='fast-toc-" + h.nodeName.toLocaleLowerCase() + "'><a href='#" + id + "'>" + h.innerText + "</a></li>");
-            h.setAttribute("id", id);
-            hCounter += 1;
-        });
-        list.push("</ul>");
+        var hCounter = 0;
+        var headers = root.querySelectorAll("h1, h2, h3, h4, h5, h6");
+
+        var listNodeName = FAST_TOC.ordered ? "ol" : "ul"
+
+        if (FAST_TOC.collapsible) {
+
+            var currentLevel = null;
+            var lastLevel = null;
+
+            list.push("<" + listNodeName + " class='fast-toc-collapsible'>");
+            // Make nested list, only works for correct hierarchy of headers
+            
+            headers.forEach(function(h, index) {
+                
+                if (h.getAttribute('data-fast-toc-ignore')) return;
+
+                var level = parseInt(h.nodeName.substr(1), 10);
+                lastLevel = currentLevel;
+                currentLevel = level;
+
+                var id = "fh-" + index;
+                h.setAttribute("id", id);
+
+                if (lastLevel !== null) {
+                    if (currentLevel > lastLevel) {
+                        list.push("<" + listNodeName + ">"); // TODO: multiply for bigger steps in level?
+                    } else if (currentLevel < lastLevel) {
+                        list.push("</" + listNodeName + "></li>"); // TODO: multiply for bigger steps in level?
+                    } else {
+                        list.push("</li>");
+                    }
+                }
+                list.push("<li><a href='#" + id + "'>" + h.innerText + "</a>");
+                
+                hCounter += 1;
+            });
+
+            list.push("</" + listNodeName + ">");
+
+        } else {
+            // Simple, works always
+            var levelStart = null;
+            list.push("<ul>");
+            headers.forEach(function(h, index) {
+                
+                if (h.getAttribute('data-fast-toc-ignore')) return;
+
+                var id = "fh-" + index;
+                var level = parseInt(h.nodeName.substr(1), 10);
+                if (index === 0) {
+                    levelStart = level;
+                }
+
+                list.push("<li class='fast-toc-" + (level - levelStart) + "'><a href='#" + id + "'>" + h.innerText + "</a></li>");
+                h.setAttribute("id", id);
+                hCounter += 1;
+            });
+            list.push("</ul>");
+        }
 
         if (hCounter === 0 || FAST_TOC.minimal_header_count && FAST_TOC.minimal_header_count > hCounter) {
             return;
